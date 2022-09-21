b72cf4f121dfc2869a3a503c3a6a4474d65c9463
sgumus <sgumus@ebay.com>
2022-09-20 21:39:46 -0500

    Solution of Romen Numerals

--- aws/projects/Project-001-Roman-Numerals-Converter/CLI_solution/userdata.sh
+++ aws/projects/Project-001-Roman-Numerals-Converter/CLI_solution/userdata.sh
@@ -2,6 +2,7 @@
 yum update -y
 yum install python3 -y
 pip3 install flask
+yum install git -y
 cd /home/ec2-user
 wget -P templates https://raw.githubusercontent.com/serdarcw/cli_deneme/master/templates/index.html
 wget -P templates https://raw.githubusercontent.com/serdarcw/cli_deneme/master/templates/result.html
